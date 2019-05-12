9c48999a4b2245cb94d07033d821c9e3883b2008
heumsi <heumsi@gmail.com>
2019-05-12 19:24:45 +0900

    홈 화면, 푸터에 스타일링 추가

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer>
-    <div>
+    <div class="copyright">
       ©2019- Heumsi.
     </div>
-    <div>
+    <div class="desc">
       <p>
         이 사이트는, 친구들에게 사랑의 5가지 언어 테스트를 좀 더 쉽게 하기위해 만들어졌습니다.<br>
         영리적 목적이 없음을 밝힙니다.
@@ -13,9 +13,7 @@
         테스트와 결과 내용의 저작권은 사이트 운영자에게 없습니다.
       </p>
       <p>
-        문제가 될만한 사항이나, 저작권 관련 문제가 있다면, 바로 조치하겠습니다.
-      </p>
-      <p>
+        문제가 될만한 사항이나, 저작권 관련 문제가 있다면, 바로 조치하겠습니다.<br>
         그 외, 기타 문의사항 및 건의사항은 heumsi@naver.com 으로 주시면 감사하겠습니다.
       </p>
     </div>
@@ -28,6 +26,15 @@ export default {
 </script>
 <style scoped>
   footer {
-    padding: 60px 0 60px 0
+    padding: 20px 0 20px 0
+  }
+  .copyright {
+    font-weight: 800;
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+  .desc {
+    font-size: 12px;
+    line-height: 180%;
   }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,34 @@
 <template>
   <div>
-  <b-jumbotron>
-    <template slot="header">사랑의 5가지 언어</template>
+    <b-jumbotron>
+      <template slot="header">사랑의 5가지 언어</template>
 
-    <template s lot="lead">
-      사랑은 언어처럼 많은 뉘앙스를 가지고 있습니다.<br>
-      1995 년, 게리 채프먼은 그가 존재한다고 믿는 5가지 사랑에 관한 언어를 설명했다. 그는 사람이 사랑을 표현하는 방식과, 그것을 받는 방식을 모두 포함하여 설명하고자 했다.
-    </template>
+      <template s lot="lead">
+        사랑은 언어처럼 많은 뉘앙스를 가지고 있습니다.
+        <br>1995 년, 게리 채프먼은 그가 존재한다고 믿는 5가지 사랑에 관한 언어를 설명했다. 그는 사람이 사랑을 표현하는 방식과, 그것을 받는 방식을 모두 포함하여 설명하고자 했다.
+      </template>
 
-    <hr class="my-4">
+      <hr class="my-4">
 
-    <p>
-      당신의 사랑의 5가지 언어. 지금 바로 검사를 통해 확인해보세요.
-    </p>
+      <p>당신의 사랑의 5가지 언어. 지금 바로 검사를 통해 확인해보세요.</p>
 
-    <b-button variant="primary" to="/test">검사 하기</b-button>
-  </b-jumbotron>
-</div>
+      <b-button variant="outline-primary" to="/test">검사 하기</b-button>
+    </b-jumbotron>
+  </div>
 </template>
 <script>
 export default {
-  name: "Home",
-}
+  name: "Home"
+};
 </script>
+<style scoped>
+.jumbotron {
+  height: 600px;
+  margin-bottom: 0;
+  color: black;
+  /*background-image: url("http://gagmagazine.co.kr/wp-content/uploads/2017/05/2017-05-26-10.19.56.png");*/
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
